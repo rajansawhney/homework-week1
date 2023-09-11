f888d53ee23e78ffa5ada3b180836c71f4b0077e
rajansawhney <rajan.k.sawhney@gmail.com>
2023-09-10 15:32:51 -1000

    assignment update

--- prob4a.c
+++ prob4a.c
@@ -7,6 +7,14 @@ printfs) with some nice comments.
 */
 
 int main() {
+    // NOTE: do not change name and type of the variables
+    int num1, num2;
+    float num3, num4;
     
+    // Read 2 integers
+
+    // Read 2 floating point numbers
+
+    // Print in following order: num1, num2, num3, num4
     return 0;
 }
