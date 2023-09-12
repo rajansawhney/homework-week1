e0c64598db4e691c2126946b73f72623a54d7556
Rajan Sawhney <11162679+rajansawhney@users.noreply.github.com>
2023-09-11 22:50:45 -0700

    Update prob4d.c

--- prob4d.c
+++ prob4d.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
-Repeat a) and b), but now print all real numbers with only 3 digits aKer the
+Repeat a) and b), but now print all real numbers with only 3 digits after the
 decimal point.
   
 */
