4c507e6fb6e944e6373bd88717c0fd9e39534111
Rajan Sawhney <11162679+rajansawhney@users.noreply.github.com>
2023-09-12 14:12:53 -0700

    Update prob3.c

--- prob3.c
+++ prob3.c
@@ -8,8 +8,8 @@ Enter values for f1 and f2:
 23.5 14.326
 Enter value for x1 and x2:
 54 7
-f1 = 23.500000, f2 =   14.33
-x1 = 54, x2 =     7 
+f1 = 23.500000, f2 = 14.33
+x1 = 54, x2 = 7 
 */
 
 int main() {
